feff7effee7753a71cc9693e9d1d05fe49ec5a9e
Sem van Elteren <sem407@outlook.com>
2023-11-15 14:08:22 +0100

    Categories CRUD

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <x-guest-layout>
+        <h1 class="mb-2 text-2xl font-bold tracking-tight text-black-100 dark:text-black">categories | {{ $category->name }}</h1>
+        <p>{{ $category->description }}</p>
+
+        <a href="{{ route('categories.index') }}" class="btn btn-primary items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-french-grey dark:hover:bg-cool-grey dark:focus:ring-black-800">Back</a>
+        @auth
+            @if(Auth::user()->id === $category->user_id || Auth::user()-> is_admin == 1 )
+                <!-- edit button -->
+                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-french-grey dark:hover:bg-cool-grey dark:focus:ring-black-800">Edit</a>
+                <!-- delete button -->
+                <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger  items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-french-grey dark:hover:bg-cool-grey dark:focus:ring-black-800">Delete</button>
+                </form>
+            @endif
+        @endauth
+    </x-guest-layout>
+@endsection
