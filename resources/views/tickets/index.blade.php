18f71d60a4db31cb597c83817fa78394f8f5dc8b
MysticExile <ethan.baboeram@gmail.com>
2024-01-08 22:37:23 +0100

    Add styling to ticket page

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <x-guest-layout>
-        <div class="container">
-            <div class="">
-                <h1>Tickets</h1>
+        <div class="">
+            <div class="justify-center">
+                <h1 class="text-6xl my-4">Tickets</h1>
                 <p>Locatie: nog onbekend</p>
                 <p>prijs: €10</p>
                 <p>datum: 10-10-2020</p>
-                <p>details: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem exercitationem harum
-                    praesentium reprehenderit sequi similique tempora voluptatibus. Accusamus animi dignissimos
+                <p>details: Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Amet autem exercitationem harum
+                    praesentium reprehenderit sequi similique tempora voluptatibus. Accusamus animi dignissimos <br>
                     distinctio
                     itaque mollitia nihil qui repellendus tenetur vero voluptates.</p>
                 <x-primary-button class="ml-4">
@@ -17,22 +17,22 @@
                 </x-primary-button>
             </div>
         </div>
-        <div class="container">
-            <h1>jouw tickets</h1>
+        <h1 class="my-2 inline-flex items-center px-3 py-2 text-xl font-medium text-center text-black bg-blue-700 focus:ring-black-300 dark:bg-french-grey dark:focus:ring-black-800">Jouw tickets</h1>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <br>
             @foreach($tickets as $ticket)
-                <div class="card">
-                    <div class="card-header">
-                        <h2>{{ $ticket->type}}</h2>
+                <div class="m-2 h-auto w-[400px] bg-white border border-gray-200 shadow dark:bg-off-white dark:border-gray-700 items-center text-center">
+                    <div class="">
+                        <h2 class="text-2xl">{{ $ticket->type}}</h2>
                     </div>
-                    <div class="card-body">
+                    <div class="">
                         <p>Datum: {{ $ticket->date }}</p>
                         <p>details {{ $ticket->description }}</p>
                         <p>Prijs: €{{ $ticket->price }}</p>
-                        <a href="{{ route('tickets.show', $ticket->id) }}" class="btn btn-primary">Bekijk Ticket</a>
+                        <a href="{{ route('tickets.show', $ticket->id) }}" class="my-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-french-grey dark:hover:bg-cool-grey dark:focus:ring-black-800">Bekijk Ticket</a>
                     </div>
                 </div>
             @endforeach
         </div>
     </x-guest-layout>
-
 @endsection
