e7f2a61019f72ee5095bc8af19f584e606ee7e17
Woefies <1062594@hr.nl>
2024-01-09 15:35:08 +0100

    dashboard redirect to products and tickets and fixed some translations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,10 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm">
                     <div class="p-6 text-black">
-                        {{ __("You're logged in!") }}
+                        <ul>
+                            <li><a href="{{ route('products.index') }}" class="font-bold underline text-3xl ">Onze Merch</a></li>
+                            <li><a href="{{ route('tickets.index') }}" class="font-bold underline text-2xl">Tickets</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- resources/views/vedutes/index.blade.php
+++ resources/views/vedutes/index.blade.php
@@ -32,7 +32,7 @@
                             <a href="{{ route('vedutes.show', $vedute->id)
                                     }}"
                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-french-grey dark:hover:bg-cool-grey dark:focus:ring-black-800">
-                                Read more
+                                Lees meer
                                 <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
