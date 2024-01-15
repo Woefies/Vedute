1f8e3d9e9f079659159e09328dab16ab291bf2c5
Sem van Elteren <1062594@hr.nl>
2024-01-15 21:43:33 +0100

    potential fix for bg not showing on live

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased bg-[{{ URL::to('/') }}/public/background.png')]">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-100 text-center bg-[url('../../../public/background.png')]">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-100 text-center bg-[{{ URL::to('/') }}/public/background.png')]">
             <div>
                 <a href="/">
                     <x-application-logo class="w-full h-full fill-current text-black-100 ml-10 pl-10" />
